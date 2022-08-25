6bab572b0a5fe8f9e19f4ef8a301691b7f1036cb
Jong44 <mat_skak45@gmail.com>
2022-08-25 09:29:56 +0700

    Progress

--- homepage.php
+++ homepage.php
@@ -5,8 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HomePage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="assets/css/style-navbar.css">
     <link rel="stylesheet" href="assets/css/style-homepage.css">
+    <link rel="stylesheet" href="assets/css/style-footer.css">
+    <script src="https://kit.fontawesome.com/860ae798ee.js" crossorigin="anonymous"></script>
+
 </head>
 <?php include 'paartials/navbar.php';?>
 
@@ -15,7 +20,7 @@
         <h3>Best <div class="merah">Service</div> </h3>
         <h3>For Your Company</h3>
         <p>Our team will always strive to give the best. For your company, both software, hardware, and server solutions.</p>
-        <a href=""><div class="btn">Contact Us</div></a>
+        <button class="btn-danger">Contact Us</button>
     </div>
     <div class="gambar">
         <div class="round">
@@ -29,9 +34,165 @@
         <h3>Our Service</h3>
         <p>We provide various services related to technology information that can fulfill your needs.</p>
     </div>
-    
+    <div class="container-fluid">
+		<div class="row">
+		    <div class="col-md-15">
+				<div id="inam" class="carousel slide carousel-dark" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="container w-300 d-block">
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="service">
+                                            <div class="icon">
+                                                <i class="fa-solid fa-chart-bar"></i>
+                                            </div>
+                                            <h5>
+                                                Business Analyst
+                                            </h5>
+                                            <p>
+                                            Providing services in the IT field with a specialization in application development supported by a team of consultants with extensive industry experience
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="service">
+                                            <div class="icon">
+                                                <i class="fa-brands fa-windows"></i>
+                                            </div>
+                                            <h5>
+                                                Quality Assurance Engineer
+                                            </h5>
+                                            <p>
+                                                Providing services in the IT field with a specialization in application development supported by a team of consultants with extensive industry experience
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="service">
+                                            <div class="icon">
+                                                <i class="fa-brands fa-windows"></i>
+                                            </div>
+                                            <h5>
+                                                IT Helpdesk
+                                            </h5>
+                                            <p>
+                                            Providing services in the IT field with a specialization in application development supported by a team of consultants with extensive industry experience
+                                            </p>
+                                        </div>
+                                    </div>
+                                    
+                                    </div>
+                                    </div>
+                                    </div>
+                                <div class="carousel-item">
+                                    <div class="container w-300 d-block">
+                                        <div class="row">
+                                        <div class="col">
+                                            <div class="service">
+                                                <div class="icon">
+                                                    <i class="fa-solid fa-cloud"></i>
+                                                </div>
+                                            <h5>
+                                                Back-End Developer
+                                            </h5>
+                                            <p>
+                                                Providing services in the IT field with a specialization in application development supported by a team of consultants with extensive industry experience
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="service">
+                                            <div class="icon">
+                                                <i class="fa-brands fa-windows"></i>
+                                            </div>
+                                            <h5>
+                                                Front-End Developer
+                                            </h5>
+                                            <p>
+                                            Providing services in the IT field with a specialization in application development supported by a team of consultants with extensive industry experience
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="service">
+                                            <div class="icon">
+                                                <i class="fa-brands fa-windows"></i>
+                                            </div>
+                                            <h5>
+                                                IT Development Service
+                                            </h5>
+                                            <p>
+                                            Providing services in the IT field with a specialization in application development supported by a team of consultants with extensive industry experience
+                                            </p>
+                                        </div>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    </div>
+                                        <a href="#inam" class="carousel-control-prev" data-bs-slide="prev">
+                                            <span class="carousel-control-prev-icon"></span>
+                                        </a>
+                                        <a href="#inam" class="carousel-control-next" data-bs-slide="next">
+                                            <span class="carousel-control-next-icon"></span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
 </div>
 
+<div class="content">
+    <div class="container">
+        <h3>Our Client</h3>
+         <div class="row">
+            <div class="col">
+                <img src="assets/img/iconplus.png" alt="Icon+">
+            </div>
+            <div class="col" style="padding-top: 20px;">
+                <img src="assets/img/ge.png" alt="Icon+" style="width:55%;">
+            </div>
+            <div class="col">
+                <img src="assets/img/cosmedic.png" alt="Icon+">
+            </div>
+         </div>
+         <div class="row">
+            <div class="col">
+                <img src="assets/img/combine.png" alt="Icon+">
+            </div>
+            <div class="col" style="padding-left: 0px;">
+                <img src="assets/img/pln.png" alt="Icon+" style="width:55%;">
+            </div>
+            <div class="col" style="padding-right: 100px;">
+                <img src="assets/img/ugmm.png" alt="Icon+" style="width:130%;">
+            </div>
+            <div class="col">
+                <img src="assets/img/tjs.png" alt="Icon+">
+            </div>
+         </div>
+    </div>
+</div>
+
+<div class="content">
+    <div class="container">
+        <h3>Trusted by Awesome Clients</h3>
+        <div class="quote-right">
+            <i class="fa-solid fa-quote-right"></i>
+        </div>
+        <div class="container1">
+            <p>
+                UGM Lecturer Study Project about news poll apk The poll was conducted to find out how the opinion about the news developed, especially in the UGM area, Mega Giga Solusindo is competent in carrying out tasks according to the deadline that we provide with satisfactory results.
+            </p>
+        </div>
+        <div class="quote-left">
+            <i class="fa-solid fa-quote-left"></i>
+        </div>
+        
+    </div>
+</div>
+
+<?php include "paartials/footer.php"; ?>
 <body>
 
 </body>
---
+++ paartials/footer.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <footer class="site-footer">
+        <div class="row">
+            <div class="col-md-5">
+                <div class="container">
+                    <img src="assets/img/mgs1.png" width="50%" alt="Mega Giga Solusindo">
+                    <p class="text-justify">Pondok Permai Wirosaban, Jl. Ki Ageng Pemanahan, Kragilan, Tamanan, Banguntapan Sub-District, Bantul Regency, Special Region of Yogyakarta 55191</p>
+                    <div class="container0">
+                    <h5>Inquire & Careers</h5> 
+                        <ul class="footer-sosial">
+                            <li>
+                                <li class="fa-solid fa-envelope"></li>
+                                    <a href="mailto:megagiga20@yahoo.com">megagiga20@yahoo.com</a>
+                                    <a href="mailto:recruitment@megagigasolusindo.co.id">recruitment@megagigasolusindo.co.id</a>
+                            </li>
+                            <li><li class="fa-solid fa-phone"></li>
+                                    <a href="https://api.whatsapp.com/send?phone=628122956948">0812 2956 948</a>
+                                    <a href="https://api.whatsapp.com/send?phone=6281288562886">0812 8856 2886</a>
+                            </li>
+                            <li><li class="fa-brands fa-firefox"></li><a href="https://megagigasolusindo.co.id/">www.megagigasolusindo.co.id</a></li>
+                        </ul> 
+                    </div>  
+                </div>  
+            </div>
+            <div class="col">
+                <div class="container1">
+                    <h5>Company</h5>
+                    <ul class="footer-links">
+                        <li><a href="">About Us</a></li>
+                        <li><a href="">Careers</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+                </div>
+                
+            </div>
+            <div class="col">
+                <div class="container2">
+                    <h5>Our Services</h5>
+                    <ul class="footer-links">
+                        <li><a href="">It Development Service</a></li>
+                        <li><a href="">Human Resource Service</a></li>
+                        <li><a href="">Project Base</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <h6><i class="fa-regular fa-copyright"></i> 2022. MEGAGIGASOLUSINDO ALL RIGHT RESERVED</h6>
+    </footer>
+</body>
+</html>
