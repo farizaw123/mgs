95f68de8968c1c0488e3571447127deb4faf9f12
farizaw <jtanpabatas@gmai.com>
2022-08-30 08:43:49 +0700

    progress 4

--- career.php
+++ career.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Career</title>
     <link rel="icon" href="./assets/img/mgs.ico">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="assets/css/style-navbar.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style-career.css">
     <link rel="stylesheet" href="assets/css/style-footer.css">
     <script src="https://kit.fontawesome.com/860ae798ee.js" crossorigin="anonymous"></script>
@@ -30,14 +30,63 @@
     </div>
 
     <div class="content">
-        <div class="judul">
-            <h1>About Job and Career</h1>
+        <div class="icon">
+            <i class="fa-solid fa-floppy-disk"></i>
         </div>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    
+        <div class="judul">
+            <h1>About <div class="merah">Job</div> and <div class="merah">Career</div></h1>
+        </div>
+    </div>
+
+    <div class="jadwal">
+        <div class="box1">
+            <div class="row row-cols-4">
+                <div class="col-sm-3">
+                    <h3>Business Analyst</h3>
+                    </i><i class="fa-solid fa-briefcase"></i>PT Mega Giga Solusindo
                 </div>
+                <div class="col-sm-3" style='margin-top: 45px'>
+                    </i><i class="fa-solid fa-location-dot"></i>Indonesia
+                </div>
+                <div class="col-sm-3">
+                    <h5>30 September 2022</h5>
+                    <p>no of vacancies</p>
+                </div>
+                <button class="btn">Contact Us</button>
+            </div>
+        </div>
+
+        <div class="box2">
+            <div class="row row-cols-4 red">
+                <div class="col-sm-3">
+                    <h3>Business Analyst</h3>
+                    </i><i class="fa-solid fa-briefcase"></i>PT Mega Giga Solusindo
+                </div>
+                <div class="col-sm-3" style='margin-top: 45px'>
+                    </i><i class="fa-solid fa-location-dot"></i>Indonesia
+                </div>
+                <div class="col-sm-3">
+                    <h5>30 September 2022</h5>
+                    <p>no of vacancies</p>
+                </div>
+                <button class="btn">Contact Us</button>
+            </div>
+        </div>
+
+        <div class="box3">
+            <div class="row row-cols-4">
+                <div class="col-sm-3">
+                    <h3>Business Analyst</h3>
+                    </i><i class="fa-solid fa-briefcase"></i>PT Mega Giga Solusindo
+                </div>
+                <div class="col-sm-3" style='margin-top: 45px'>
+                    </i><i class="fa-solid fa-location-dot"></i>Indonesia
+                </div>
+                <div class="col-sm-3">
+                    <h5>30 September 2022</h5>
+                    <p>no of vacancies</p>
+                </div>
+                <button class="btn">Contact Us</button>
             </div>
         </div>
     </div>
